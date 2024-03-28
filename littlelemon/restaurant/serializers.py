14902c1a2fd0a5a8e90d81066f2e6dc4239b6d6f
Knut Edvard Kjersem <knutekjersem@gmail.com>
2024-03-29 00:15:07 +0100

    final

--- littlelemon/restaurant/serializers.py
+++ littlelemon/restaurant/serializers.py
@@ -2,14 +2,19 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from datetime import datetime
 from .models import Booking, Menu   
-
+from django.contrib.auth.models import User
 
 class MenuSerializer(serializers.ModelSerializer):
     class Meta:
         model = Menu
-        fields = "__all__"
+        fields = ['id','title','price','inventory']
         
 class BookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Booking
         fields = "__all__"
+        
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from rest_framework.response import Response
-from rest_framework import  generics
-from .models import Booking, Menu
-from .serializers import BookingSerializer, MenuSerializer
+from rest_framework.decorators import api_view
+from rest_framework import generics
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
-
-# Create your views here.
-def index(request):
-    return render(request, 'index.html', {})
+from rest_framework import permissions, viewsets
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from .serializers import BookingSerializer, UserSerializer, MenuSerializer
+from .models import Menu, Booking
 
 
 class BookingViewSet (ModelViewSet):
@@ -26,5 +27,9 @@ class MenuSingleView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
    
-
+@api_view()
+@permission_classes([IsAuthenticated])
+# @authentication_classes([TokenAuthentication])
+def msg(request):
+    return Response({"message":"This view is protected"})
     
